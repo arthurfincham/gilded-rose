5e993369ff1d4a78c9f4ca82e1640e0bb83ca621
Arthur Fincham <arthurfincham@me.com>
2021-11-23 18:13:07 +0000

    extract brie method into class

---
+++ lib/aged_brie.rb
@@ -0,0 +1,11 @@
+require 'standard_item'
+class AgedBrie < StandardItem
+
+  def update
+    item.quality += 2 if item.sell_in <= 0
+    item.quality += 1 if item.sell_in > 0
+    item.sell_in -= 1
+    quality_cap(item)
+  end
+
+end
