a30d3964907109de98b25fc3f86f25378c535081
Arthur Fincham <arthurfincham@me.com>
2021-11-23 18:13:20 +0000

    extract standard update into class

---
+++ lib/standard_item.rb
@@ -0,0 +1,22 @@
+class StandardItem
+
+  attr_accessor :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def update
+    item.quality -= 2 if item.sell_in <= 0
+    item.quality -= 1 if item.sell_in > 0
+    item.sell_in -= 1 
+    quality_cap(item)
+  end
+
+  private
+
+  def quality_cap(item)
+    item.quality = 0 if item.quality < 0
+    item.quality = 50 if item.quality > 50
+  end
+end
---
+++ spec/standard_item_spec.rb
@@ -0,0 +1,28 @@
+require "standard_item"
+require "mock_item"
+
+describe StandardItem do
+  context "sell_in is not negative" do
+    subject { described_class.new(MockItem.new("Standard", 10, 10)) }
+    it "reduces quality by 1" do
+      expect { subject.update }.to change { subject.item.quality }.by(-1)
+    end
+
+    it "reduces sell_in by 1" do
+      expect { subject.update }.to change { subject.item.sell_in }.by(-1)
+    end
+  end
+
+  context "sell_in is negative" do
+    subject { described_class.new(MockItem.new("Standard", -1, 2)) }
+    it 'reduces quality by 2' do
+      expect { subject.update }.to change {subject.item.quality }.by(-2)
+    end
+
+    it 'does not reduce quality below 0' do
+      subject.update
+      subject.update
+      expect(subject.item.quality).to eq 0
+    end
+  end
+end
