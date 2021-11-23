443b4e7d296af4b4777f42f3c824b28c1a8af789
Arthur Fincham <arthurfincham@me.com>
2021-11-23 09:34:02 +0000

    add fixture test case

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -31,6 +31,13 @@ class GildedRose
     item.quality -= 2 if item.quality > 1 && item.sell_in < 0
   end
 
+  def update_item(item)
+    unless special_item?(item)
+      update_days(item)
+      decrease_quality(item)
+    end
+  end
+
   def update_quality()
     @items.each do |item|
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
