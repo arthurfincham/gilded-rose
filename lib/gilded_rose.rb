6d57c02551bcfc84fe28aca60d7a9e2a7335498c
Arthur Fincham <arthurfincham@me.com>
2021-11-23 09:59:30 +0000

    remove .special_item method

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -12,15 +12,6 @@ class GildedRose
     item.sell_in -= 1
   end
 
-  def special_item?(item)
-    case
-    when item.name.include?("Sulfuras") || item.name.include?("Aged Brie") || item.name.include?("Backstage passes")
-      true
-    else
-      false
-    end
-  end
-
   def increase_quality(item)
     item.quality += 1 if item.quality < 50
   end
@@ -36,7 +27,7 @@ class GildedRose
     when item.sell_in <= 0
       item.quality = 0
     when item.sell_in <= 5
-      3.times{increase_quality(item)}
+      3.times { increase_quality(item) }
     when item.sell_in <= 10
       2.times { increase_quality(item) }
     else
@@ -45,57 +36,24 @@ class GildedRose
   end
 
   def update_item(item)
-    unless special_item?(item)
+    case
+    when item.name.include?("Sulfuras")
+      item
+    when item.name.include?("Aged Brie")
+      update_days(item)
+      increase_quality(item)
+    when item.name.include?("Backstage passes")
+      update_days(item)
+      pass_quality(item)
+    else
       update_days(item)
       decrease_quality(item)
     end
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      update_item(item)
     end
   end
 end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -11,27 +11,6 @@ describe GildedRose do
     end
   end
 
-  describe ".special_item?" do
-    it "returns true for Aged Brie" do
-      item = MockItem.new("Aged Brie", 3, 6)
-      expect(subject.special_item?(item)).to be true
-    end
-
-    it 'returns true for Sulfuras' do
-      item = MockItem.new("Sulfuras", 3, 6)
-      expect(subject.special_item?(item)).to be true
-    end
-
-    it 'returns true for Backstage passes' do
-      item = MockItem.new("Backstage passes", 4, 4)
-      expect(subject.special_item?(item)).to be true
-    end
-
-    it 'returns false for any other item' do
-      item = MockItem.new("Chair", 1, 4)
-      expect(subject.special_item?(item)).to be false
-    end
-  end
 
   describe ".increase_quality" do
     it 'increase the quality of an item' do
