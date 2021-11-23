b8c5f8e729c4b1b2569b72c8e5e35d04559f0182
Arthur Fincham <arthurfincham@me.com>
2021-11-23 17:03:02 +0000

    change generic item names to Standard Item in spec

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,22 +2,22 @@ require "gilded_rose"
 require "mock_item"
 
 describe GildedRose do
-  subject { described_class.new(MockItem.new("Cake", 3, 6)) }
+  subject { described_class.new(MockItem.new("Standard Item", 3, 6)) }
   describe ".update_days" do
     it "reduces the days remaining for the item" do
-      item = MockItem.new("Cake", 3, 6)
+      item = MockItem.new("Standard Item", 3, 6)
       expect { subject.update_days(item) }.to change { item.sell_in }.by (-1)
     end
   end
 
   describe ".increase_quality" do
     it "increase the quality of an item" do
-      item = MockItem.new("Chair", 1, 4)
+      item = MockItem.new("Standard Item", 1, 4)
       expect { subject.increase_quality(item) }.to change { item.quality }.by (1)
     end
 
     it "does not increase quality past 50" do
-      item = MockItem.new("Chair", 1, 50)
+      item = MockItem.new("Standard Item", 1, 50)
       expect { subject.increase_quality(item) }.to change { item.quality }.by (0)
     end
 
@@ -29,22 +29,22 @@ describe GildedRose do
 
   describe ".decrease_quality" do
     it "reduces the quality of an item" do
-      item = MockItem.new("Chair", 1, 5)
+      item = MockItem.new("Standard Item", 1, 5)
       expect { subject.decrease_quality(item) }.to change { item.quality }.by (-1)
     end
 
     it "prevents item quality being negative" do
-      item = MockItem.new("Chair", 1, 0)
+      item = MockItem.new("Standard Item", 1, 0)
       expect { subject.decrease_quality(item) }.to change { item.quality }.by (0)
     end
 
     it "decreases twice as fast if sell_in = 0" do
-      item = MockItem.new("Chair", -1, 5)
+      item = MockItem.new("Standard Item", -1, 5)
       expect { subject.decrease_quality(item) }.to change { item.quality }.by (-2)
     end
 
     it "negative sell_in does not make quality negative" do
-      item = MockItem.new("Chair", -1, 1)
+      item = MockItem.new("Standard Item", -1, 1)
       subject.decrease_quality(item)
       expect(item.quality).to eq 0
     end
@@ -93,8 +93,6 @@ describe GildedRose do
       expect { subject.conjured_quality(item) }.to change { item.quality }.by (-4)
     end
   end
-
-  
 
   describe ".update_item" do
     context "Sulfuras returns itself" do
@@ -152,27 +150,24 @@ describe GildedRose do
     end
     context "Normal Item calls" do
       it ".update_days" do
-        item = MockItem.new("Banana", 10, 10)
+        item = MockItem.new("Normal Item", 10, 10)
         expect(subject).to receive(:update_days).with(item)
         subject.update_item(item)
       end
 
       it ".decrease_quality" do
-        item = MockItem.new("Banana", 10, 10)
+        item = MockItem.new("Normal Item", 10, 10)
         expect(subject).to receive(:decrease_quality).with(item)
         subject.update_item(item)
       end
     end
-  end
+    it "gold standard == test.txt" do
+      `ruby spec/texttest_fixtures.rb 20 > test.txt`
+      expected = "spec/gold_standard.txt"
+      actual = "test.txt"
 
-describe "#update_quality" do
-  it "has no regression" do
-    `ruby spec/texttest_fixtures.rb 20 > test.txt`
-    expected = "spec/gold_standard.txt"
-    actual = "test.txt"
-
-    expect(IO.read(actual)).to eq IO.read(expected)
-    `rm test.txt`
+      expect(IO.read(actual)).to eq IO.read(expected)
+      `rm test.txt`
+    end
   end
-end
 end
