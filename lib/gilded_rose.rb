399b4a56ae089c5514f907d40dadbfbb8c42ee84
Arthur Fincham <arthurfincham@me.com>
2021-11-23 09:50:18 +0000

    refactor .pass_quality

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -36,11 +36,11 @@ class GildedRose
     when item.sell_in <= 0
       item.quality = 0
     when item.sell_in <= 5
-      item.quality += 3
+      3.times{increase_quality(item)}
     when item.sell_in <= 10
-      item.quality += 2
+      2.times { increase_quality(item) }
     else
-      item.quality += 1
+      increase_quality(item)
     end
   end
 
