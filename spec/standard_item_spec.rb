41bef9dbf9e0d7c566f3e6c156e9b4cb23395f93
Arthur Fincham <arthurfincham@me.com>
2021-11-24 10:16:12 +0000

    refactor standard item spec

--- spec/standard_item_spec.rb
+++ spec/standard_item_spec.rb
@@ -1,22 +1,22 @@
-require "standard_item"
-require "mock_item"
+require 'standard_item'
+require 'mock_item'
 
 describe StandardItem do
-  context "sell_in is not negative" do
-    subject { described_class.new(MockItem.new("Standard", 10, 10)) }
-    it "reduces quality by 1" do
+  context 'sell_in is not negative' do
+    subject { described_class.new(MockItem.new('Standard', 10, 10)) }
+    it 'reduces quality by 1' do
       expect { subject.update }.to change { subject.item.quality }.by(-1)
     end
 
-    it "reduces sell_in by 1" do
+    it 'reduces sell_in by 1' do
       expect { subject.update }.to change { subject.item.sell_in }.by(-1)
     end
   end
 
-  context "sell_in is negative" do
-    subject { described_class.new(MockItem.new("Standard", -1, 2)) }
+  context 'sell_in is negative' do
+    subject { described_class.new(MockItem.new('Standard', -1, 2)) }
     it 'reduces quality by 2' do
-      expect { subject.update }.to change {subject.item.quality }.by(-2)
+      expect { subject.update }.to change { subject.item.quality }.by(-2)
     end
 
     it 'does not reduce quality below 0' do
