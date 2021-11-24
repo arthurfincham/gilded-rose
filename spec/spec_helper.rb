4cb939094e45bbbf83c6e9e1b530c83b8ffd73bb
Arthur Fincham <arthurfincham@me.com>
2021-11-24 10:16:05 +0000

    refactor spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,7 @@
-require "simplecov"
-require "simplecov-console"
+require 'simplecov'
+require 'simplecov-console'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::Console,
-])
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([SimpleCov::Formatter::Console])
 SimpleCov.start
 
 RSpec.configure do |config|
