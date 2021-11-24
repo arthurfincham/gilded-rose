cbdce182a2437a727e6532b33a613d5a560f123f
Arthur Fincham <arthurfincham@me.com>
2021-11-24 10:09:36 +0000

    refactor gilded rose

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,9 +1,9 @@
-require "./lib/item"
-require "./lib/aged_brie"
-require "./lib/conjured_item"
-require "./lib/legendary_item"
-require "./lib/standard_item"
-require "./lib/backstage_pass"
+require './lib/item'
+require './lib/aged_brie'
+require './lib/conjured_item'
+require './lib/legendary_item'
+require './lib/standard_item'
+require './lib/backstage_pass'
 
 class GildedRose
   attr_accessor :items
@@ -20,16 +20,16 @@ class GildedRose
 
   def item_class(name)
     case
-      when name.include?("Sulfuras")
-        LegendaryItem
-      when name.include?("Aged Brie")
-        AgedBrie
-      when name.include?("Backstage passes")
-        BackstagePass
-      when name.include?("Conjured")
-        ConjuredItem
-      else
-        StandardItem
-      end
+    when name.include?('Sulfuras')
+      LegendaryItem
+    when name.include?('Aged Brie')
+      AgedBrie
+    when name.include?('Backstage passes')
+      BackstagePass
+    when name.include?('Conjured')
+      ConjuredItem
+    else
+      StandardItem
+    end
   end
 end
