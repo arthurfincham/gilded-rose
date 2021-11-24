8440e26c66dda12f9a703bdc970601dd273eb905
Arthur Fincham <arthurfincham@me.com>
2021-11-24 10:15:57 +0000

    refactor mock_item

--- spec/mock_item.rb
+++ spec/mock_item.rb
@@ -1,10 +1,9 @@
 class MockItem
   attr_accessor :name, :sell_in, :quality
-  
+
   def initialize(name, sell_in, quality)
     @name = name
     @sell_in = sell_in
     @quality = quality
   end
-
 end
